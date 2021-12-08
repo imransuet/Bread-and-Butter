2f7cfb1bfc8cfcdc6acbb342d1c80aa8c38666dc
imransuet <imransuet@gmail.com>
2021-12-08 22:42:07 +0100

    parsing templates

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+
+	renderTemplate(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
@@ -45,6 +47,14 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error while parsing the template", err)
+	}
+}
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
